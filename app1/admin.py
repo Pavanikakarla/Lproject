84b30ce35fe895eaefe699340d4db40248912ccf
pyking004 <pyking004@gmail.com>
2023-06-14 22:09:24 +0530

    second commit

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app1.models import Course
+from app1.models import Course,Student
 # Register your models here.
 
 @admin.register(Course)
@@ -7,3 +7,8 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ['id','cname','dur','fee','cdate','trainer']
 
 # admin.site.register(Course)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id','sname','email','portfolio','pic','resume','join_date','cname']
--- app1/models.py
+++ app1/models.py
@@ -12,3 +12,15 @@ class Course(models.Model):
     def __str__(self):
         return self.cname
     
+class Student(models.Model):
+    sname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    portfolio = models.URLField(max_length=150)
+    pic = models.ImageField(upload_to='media/app1/images')
+    resume = models.FileField(upload_to='media/app1/files')
+    join_date = models.DateField(auto_now=True)
+    cname = models.ForeignKey("Course",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.sname
+
